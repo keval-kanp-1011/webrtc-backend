0d6e9d47688ee90c8e5a183c20a4c97ec79fa777
Keval <kevalkanpariya5051@gmail.com>
2023-05-15 18:39:30 +0530

    done..

--- src/main/kotlin/dev/kevalkanpariya/plugins/Routing.kt
+++ src/main/kotlin/dev/kevalkanpariya/plugins/Routing.kt
@@ -1,11 +1,7 @@
 package dev.kevalkanpariya.plugins
 
 import dev.kevalkanpariya.SessionManager
-import dev.kevalkanpariya.utils.FIREBASE_AUTH
-import dev.kevalkanpariya.utils.User
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
@@ -17,21 +13,12 @@ import java.util.*
 fun Application.configureRouting() {
     routing {
         rootRoute()
-        authenticatedRoute()
         webSocket()
     }
 
 }
 
-fun Route.authenticatedRoute() {
-    authenticate(FIREBASE_AUTH) {
-        get("/authenticated") {
-            val user: User =
-                call.principal() ?: return@get call.respond(HttpStatusCode.Unauthorized)
-            call.respond("User is authenticated: $user")
-        }
-    }
-}
+
 
 fun Route.rootRoute() {
     get("/") {
@@ -41,30 +28,28 @@ fun Route.rootRoute() {
 
 
 fun Route.webSocket() {
-    authenticate(FIREBASE_AUTH) {
-        webSocket("/rtc") {
-            val sessionID = UUID.randomUUID()
-            try {
-                SessionManager.onSessionStarted(sessionID, this)
+    webSocket("/rtc") {
+        val sessionID = UUID.randomUUID()
+        try {
+            SessionManager.onSessionStarted(sessionID, this)
 
-                for (frame in incoming) {
-                    when (frame) {
-                        is Frame.Text -> {
-                            SessionManager.onMessage(sessionID, frame.readText())
-                        }
-
-                        else -> Unit
+            for (frame in incoming) {
+                when (frame) {
+                    is Frame.Text -> {
+                        SessionManager.onMessage(sessionID, frame.readText())
                     }
+
+                    else -> Unit
                 }
-                println("Exiting incoming loop, closing session: $sessionID")
-                SessionManager.onSessionClose(sessionID)
-            } catch (e: ClosedReceiveChannelException) {
-                println("onClose $sessionID")
-                SessionManager.onSessionClose(sessionID)
-            } catch (e: Throwable) {
-                println("onError $sessionID $e")
-                SessionManager.onSessionClose(sessionID)
             }
+            println("Exiting incoming loop, closing session: $sessionID")
+            SessionManager.onSessionClose(sessionID)
+        } catch (e: ClosedReceiveChannelException) {
+            println("onClose $sessionID")
+            SessionManager.onSessionClose(sessionID)
+        } catch (e: Throwable) {
+            println("onError $sessionID $e")
+            SessionManager.onSessionClose(sessionID)
         }
     }
 }
